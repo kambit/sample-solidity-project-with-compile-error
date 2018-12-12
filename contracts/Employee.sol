00ef93400f0b1f009c4143c07edaf12838a3480e
kambit <xoiiikoocoding@gmail.com>
2018-12-13 01:04:17 +1100

    upload contracts/

---
+++ contracts/Employee.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.25;
+
+import "./Company.sol";
+
+contract Employee is Company {
+    function logHour(uint8 _hour) public {
+        require(isEmployee[msg.sender]);
+        employeeToHoursWorked[msg.sender] += _hour;
+    }
+}
